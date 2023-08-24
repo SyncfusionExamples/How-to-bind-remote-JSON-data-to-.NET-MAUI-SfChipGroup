21b5219c2cd430e53ca6155eb9c6a505a0b6bb9a
Arunachalam13 <karunachalam406@gmail.com>
2023-08-24 11:55:47 +0530

    Added the sample to the repository

---
+++ ChipGroupSample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ChipGroupSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ChipGroupSample/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ChipGroupSample;
+
+public partial class MainPage : ContentPage
+{
+	
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+}
+
